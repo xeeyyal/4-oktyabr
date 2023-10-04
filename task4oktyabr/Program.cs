c24dae68122f747a151aefdbf4071b914ab37dc8
xeeyyal <tu5luzvdh@code.edu.az>
2023-10-04 21:53:52 +0400

    4oktyabr my tasks

---
+++ task4oktyabr/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace task4oktyabr
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //TASK-1
+            //int[] arr = {56,72,11,87,52};
+            //int min = Min(arr);
+            //Console.WriteLine("En kicik element: " + min);
+
+            //TASK-2
+            //double cevreRadius = 6.0;
+            //double duzbucaqlia = 4.0;
+            //double duzbucaqlib = 8.0;
+            //double parallelepipedLength = 4.0;
+            //double parallelepipedWidth = 3.0;
+            //double parallelepipedHeight = 5.0;
+            //double ucbucaqA = 6.0;
+            //double ucbucaqB = 8.0;
+            //double ucbucaqC = 9.0;
+            //Console.WriteLine("Çevrenin sahəsi: " + Area(cevreRadius));
+            //Console.WriteLine("Düzbucaqlının sahəsi: " + Area(duzbucaqlia, duzbucaqlib));
+            //Console.WriteLine("Düzbucaqlı paralelpipedin tam səhinin sahəsi: " + Area(parallelepipedLength, parallelepipedWidth, parallelepipedHeight));
+            //Console.WriteLine("Üçbucaqlının daxilinə çəkilmiş çevrənin sahəsi: " + Area(ucbucaqA, ucbucaqB, ucbucaqC));
+
+            //TASK-3 YAZA BILMEDIM
+        }
+        //TASK-1
+        //public static int Min(int[] arr)
+        //{
+        //    int min = arr[0];
+        //    for (int i = 1; i < arr.Length; i++)
+        //    {
+        //       if (arr[i] < min) min = arr[i];
+        //    }
+        //    return min;
+        //}
+
+        //TASK-2
+        //const double Pi = 3.0;
+        //public static double Area(double radius)
+        //{
+        //    return Pi * radius * radius;
+        //}
+        //public static double Area(double a, double b)
+        //{
+        //    return a * b;
+        //}
+        //public static double Area(double length, double width, double height)
+        //{
+        //    return 2 * (length * width + length * height + width * height);
+        //}
+        //public static double Area(double A, double B, double C)
+        //{
+        //    double s = (A + B + C) / 2;
+        //    return Pi * Math.Sqrt(s * (s - A) * (s - B) * (s - C));
+        //}
+    }
+}
